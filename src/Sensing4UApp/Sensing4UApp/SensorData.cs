9681d8a8f79845c356cba85233079f61103176af
sunjudoit <lovelysupergirl314@gmail.com>
2025-11-10 14:54:54 +0800

    Added SensorData class

--- src/Sensing4UApp/Sensing4UApp/SensorData.cs
+++ src/Sensing4UApp/Sensing4UApp/SensorData.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Sensing4UApp
 {
-    internal class SensorData
+    /// <summary>
+    /// The SensorData class represents a single row(Label , Value )of sensor information.
+    /// This class is used across the application to store and manage individual sensor readings.
+    /// </summary>
+    public class SensorData
     {
+        /// <summary>
+        /// Allows reading and writing of the sensor’s label or ID.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Allows reading and writing of the sensor’s value.
+        /// </summary>
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Constructor used to initialize a new SensorData object with a label and value.
+        /// </summary>
+        /// <param name="label">The ID of the sensor.</param>
+        /// <param name="value">The recorded value for the sensor.</param>
+        public SensorData(string label, double value) 
+        {
+            Label = label;
+            Value = value;
+        }
+
+        /// <summary>
+        /// This method is used when modifying sensor readings within the DataProcessor.
+        /// </summary>
+        /// <param name="newValue">The new value to update to this sensor.</param>
+        public void SetValue(double newvalue)
+        {
+            Value = newvalue;
+
+        }
     }
+
+
 }
